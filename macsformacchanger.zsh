43b699f6aecdd5b0e2c5556e9245f3a2aa02a314
L33T <271813410+chiefbigrubberduck@users.noreply.github.com>
2026-04-08 16:53:08 +0200

    Update macsformacchanger.zsh

--- macsformacchanger.zsh
+++ macsformacchanger.zsh
@@ -7,7 +7,6 @@ INTERFACE="Your WiFi Adapter!!"
 sudo ifconfig "$INTERFACE" down
 
 # 2. Change the MAC
-# Note: If your Homebrew macchanger version complains about -b, just remove it.
-sudo macchanger -r -b "$INTERFACE"
+sudo macchanger -r "$INTERFACE"
 
 echo "MAC address changed, WiFi is still turned off, you can turn it back on through your main menu."
